fd9ca7f0fb1f661001a356e063451f489faa4297
James Turnbulls <james@lovedthanlost.net>
2025-06-26 14:53:53 -0400

    Adjusted implementation to stop generating the extra c

--- CapsC/AppDelegate.swift
+++ CapsC/AppDelegate.swift
@@ -20,7 +20,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var eventHotKeyRef: EventHotKeyRef?
     var eventHandler: EventHandlerRef?
     var f19Pressed = false
-    var monitor: Any?
+    var globalMonitor: Any?
+    var localMonitor: Any?
+    var eventTap: CFMachPort?
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         print("=== CapsC applicationDidFinishLaunching ===")
@@ -70,24 +72,83 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func registerGlobalHotkey() {
-        print("Registering F19+C hotkey...")
+        print("Registering F19+C hotkey using CGEvent tap...")
         
-        monitor = NSEvent.addGlobalMonitorForEvents(matching: [.keyDown, .keyUp]) { event in
-            if event.keyCode == 80 { // F19 key code
-                if event.type == .keyDown {
-                    self.f19Pressed = true
-                    print("F19 pressed")
-                } else if event.type == .keyUp {
-                    self.f19Pressed = false
-                    print("F19 released")
-                }
-            } else if event.keyCode == 8 && event.type == .keyDown && self.f19Pressed { // C key code
-                print("F19+C detected!")
-                self.handleHotKeyPressed()
+        // Create event tap to intercept key events
+        let eventMask = (1 << CGEventType.keyDown.rawValue) | (1 << CGEventType.keyUp.rawValue)
+        
+        eventTap = CGEvent.tapCreate(
+            tap: .cgSessionEventTap,
+            place: .headInsertEventTap,
+            options: .defaultTap,
+            eventsOfInterest: CGEventMask(eventMask),
+            callback: { (proxy, type, event, refcon) -> Unmanaged<CGEvent>? in
+                let appDelegate = Unmanaged<AppDelegate>.fromOpaque(refcon!).takeUnretainedValue()
+                return appDelegate.handleCGEvent(proxy: proxy, type: type, event: event)
+            },
+            userInfo: Unmanaged.passUnretained(self).toOpaque()
+        )
+        
+        if let eventTap = eventTap {
+            let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+            CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+            CGEvent.tapEnable(tap: eventTap, enable: true)
+            print("Successfully created CGEvent tap for F19+C")
+        } else {
+            print("Failed to create event tap, falling back to NSEvent monitoring")
+            fallbackToEventMonitoring()
+        }
+    }
+    
+    func handleCGEvent(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent) -> Unmanaged<CGEvent>? {
+        let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
+        
+        if keyCode == 80 { // F19 key code
+            if type == .keyDown {
+                self.f19Pressed = true
+                print("F19 pressed")
+            } else if type == .keyUp {
+                self.f19Pressed = false
+                print("F19 released")
             }
+            return Unmanaged.passUnretained(event) // Pass through F19 events
+        } else if keyCode == 8 && type == .keyDown && self.f19Pressed { // C key code
+            print("F19+C detected! Consuming event.")
+            self.handleHotKeyPressed()
+            return nil // Consume the C key event
         }
         
-        print("Global monitor registered for F19+C")
+        return Unmanaged.passUnretained(event) // Pass through all other events
+    }
+    
+    func fallbackToEventMonitoring() {
+        print("Using fallback NSEvent monitoring...")
+        
+        globalMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.keyDown, .keyUp]) { event in
+            _ = self.handleKeyEvent(event)
+        }
+        
+        localMonitor = NSEvent.addLocalMonitorForEvents(matching: [.keyDown, .keyUp]) { event in
+            return self.handleKeyEvent(event)
+        }
+    }
+    
+    func handleKeyEvent(_ event: NSEvent) -> NSEvent? {
+        if event.keyCode == 80 { // F19 key code
+            if event.type == .keyDown {
+                self.f19Pressed = true
+                print("F19 pressed")
+            } else if event.type == .keyUp {
+                self.f19Pressed = false
+                print("F19 released")
+            }
+            return event
+        } else if event.keyCode == 8 && event.type == .keyDown && self.f19Pressed { // C key code
+            print("F19+C detected!")
+            self.handleHotKeyPressed()
+            return nil
+        }
+        return event
     }
     
     func handleHotKeyPressed() {
@@ -143,8 +204,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
-        if let monitor = monitor {
-            NSEvent.removeMonitor(monitor)
+        if let eventTap = eventTap {
+            CGEvent.tapEnable(tap: eventTap, enable: false)
+            CFMachPortInvalidate(eventTap)
+        }
+        if let globalMonitor = globalMonitor {
+            NSEvent.removeMonitor(globalMonitor)
+        }
+        if let localMonitor = localMonitor {
+            NSEvent.removeMonitor(localMonitor)
         }
         if let eventHandler = eventHandler {
             RemoveEventHandler(eventHandler)
